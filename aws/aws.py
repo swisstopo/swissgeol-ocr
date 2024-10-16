a0bea39bd128e018f93709affcbfd6025b87987f
Daniel von Atzigen <daniel.vonatzigen@ebp.ch>
2024-10-16 13:12:10 +0200

    Remove aws session by tokens

--- aws/aws.py
+++ aws/aws.py
@@ -21,25 +21,9 @@ class Client:
 
 def connect(settings: ApiSettings) -> Client:
     has_profile = is_set(settings.aws_profile)
-    has_access_key = is_set(settings.aws_access_key)
-    has_secret_access_key = is_set(settings.aws_secret_access_key)
-
-    if (has_access_key and not has_secret_access_key) or (not has_access_key and has_secret_access_key):
-        print("Either both or none of AWS_ACCESS_KEY and AWS_SECRET_ACCESS_KEY must be set.")
-        exit(1)
-
-    if has_profile and has_access_key:
-        print("Can't use both AWS_PROFILE and secret keys.")
-        exit(1)
 
     if has_profile:
         session = open_session_by_profile(settings.aws_profile)
-    elif has_access_key:
-        session = open_session_by_access_keys(
-            settings.aws_access_key,
-            settings.aws_secret_access_key,
-            settings.aws_region,
-        )
     else:
         session = open_session_by_service_role()
 
@@ -55,14 +39,6 @@ def is_set(value: str | None) -> bool:
 
 def open_session_by_profile(profile: str) -> boto3.Session:
     return boto3.Session(profile_name=profile)
-
-
-def open_session_by_access_keys(access_key: str, secret_access_key: str, region: str) -> boto3.Session:
-    return boto3.Session(
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_access_key,
-        region_name=region,
-    )
 
 
 def open_session_by_service_role() -> boto3.Session:

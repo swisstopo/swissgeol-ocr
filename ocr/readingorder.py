b40085807b334a6055c074bf168dd3352f30cd38
Stijn Vermeeren <stijn.vermeeren@swisstopo.ch>
2026-04-14 17:17:39 +0200

    refactor readingorder.py with state class

--- ocr/readingorder.py
+++ ocr/readingorder.py
@@ -175,23 +175,49 @@ def starting_line_for_next_block(remaining_lines: set[TextLineReadingOrder]) -> 
     return selected_line
 
 
+@dataclass
+class CreateBlockState:
+    current_line: TextLineReadingOrder
+    current_block: list[TextLineReadingOrder]
+    remaining_lines: set[TextLineReadingOrder]
+    all_lines: set[TextLineReadingOrder]
+
+    def current_column(self):
+        return ReadingOrderColumn.current_column(self.current_line, self.current_block[:-1], self.all_lines)
+
+    def add_line(self, next_line: TextLineReadingOrder) -> "CreateBlockState":
+        return CreateBlockState(
+            next_line,
+            current_block=self.current_block + [next_line],
+            remaining_lines=self.remaining_lines.difference({next_line}),
+            all_lines=self.all_lines
+        )
+
+    @classmethod
+    def start(cls, start_line: TextLineReadingOrder, available_lines: set[TextLineReadingOrder], all_lines: set[TextLineReadingOrder]) -> "CreateBlockState":
+        if start_line in available_lines:
+            available_lines.remove(start_line)
+        return CreateBlockState(
+            current_line=start_line,
+            current_block=[start_line],
+            remaining_lines=available_lines,
+            all_lines=all_lines
+        )
+
 def create_block_from_line(
-        start_line: TextLineReadingOrder,
-        all_lines: set[TextLineReadingOrder],
-        remaining_lines: set[TextLineReadingOrder],
+        state: CreateBlockState,
         interrupt_lines: set[TextLineReadingOrder]
 ) -> tuple[ReadingOrderBlock | None, set[TextLineReadingOrder]]:
-    current_block = [start_line]
-    current_line = start_line
-    remaining_lines_input = remaining_lines.copy()
+    fallback_remaining_lines = state.remaining_lines.copy()
+    fallback_remaining_lines.add(state.current_line)
 
-    while remaining_lines:
+    while state.remaining_lines:
         next_line = None
 
         # add text lines that seem to continue the current column, even if they are further down (but not futher
         # down than the current height of the column)
-        column = ReadingOrderColumn.current_column(current_line, current_block[:-1], all_lines)
-        in_column_lines = {line for line in remaining_lines if column.can_be_extended_by(line.geometry)}
+        column = state.current_column()
+        in_column_lines = {line for line in state.remaining_lines if column.can_be_extended_by(line.geometry)}
         if len(in_column_lines):
             highest_following = min(in_column_lines, key=lambda line: line.geometry.rect.y0)
             candidates = {
@@ -203,7 +229,7 @@ def create_block_from_line(
 
         if not next_line:
             # lines that are directly below the last line, either left-aligned, right-aligned or centered
-            following = {line for line in remaining_lines if line.geometry.distance_after(current_line.geometry) < 20}
+            following = {line for line in state.remaining_lines if line.geometry.distance_after(state.current_line.geometry) < 20}
             if len(following):
                 next_line = min(following, key=lambda line: line.geometry.rect.y0)
 
@@ -213,33 +239,31 @@ def create_block_from_line(
         if next_line in interrupt_lines:
             break
 
-        if any(line.geometry.needs_to_come_before(next_line.geometry) for line in remaining_lines):
-            interrupting_lines = {line for line in remaining_lines if line.geometry.needs_to_come_before(next_line.geometry)}
+        interrupting_lines = {line for line in state.remaining_lines if line.geometry.needs_to_come_before(next_line.geometry)}
+        if interrupting_lines:
             new_start_line = starting_line_for_next_block(interrupting_lines)
-            if new_start_line in interrupt_lines:
-                break
-            # Start again with the line that interrupted the current column. Break once we hit a line that was already added to the current column.
-            remaining_lines_input.remove(new_start_line)
-            remaining_lines_input.add(start_line)
-            return create_block_from_line(new_start_line, all_lines, remaining_lines_input, interrupt_lines=set(current_block))
+            if new_start_line:
+                if new_start_line in interrupt_lines:
+                    break
+                # Start again with the line that interrupted the current column. Break once we hit a line that was already added to the current column.
+                new_state = CreateBlockState.start(new_start_line, fallback_remaining_lines, state.all_lines)
+                return create_block_from_line(new_state, interrupt_lines=interrupt_lines.union(state.current_block))
 
-        current_line = next_line
-        remaining_lines.remove(current_line)
-        current_block.append(current_line)
+        state = state.add_line(next_line)
 
-    return ReadingOrderBlock([line.line for line in current_block]), remaining_lines
+    return ReadingOrderBlock([line.line for line in state.current_block]), state.remaining_lines
 
 
 def create_block(
         all_lines: set[TextLineReadingOrder],
-        remaining_lines: set[TextLineReadingOrder]
+        available_lines: set[TextLineReadingOrder]
 ) -> tuple[ReadingOrderBlock | None, set[TextLineReadingOrder]]:
-    current_line = starting_line_for_next_block(remaining_lines)
+    current_line = starting_line_for_next_block(available_lines)
     if not current_line:
         return None, set()
 
-    remaining_lines.remove(current_line)
-    return create_block_from_line(current_line, all_lines, remaining_lines, interrupt_lines=set())
+    state = CreateBlockState.start(current_line, available_lines, all_lines)
+    return create_block_from_line(state, interrupt_lines=set())
 
 def sort_lines(text_lines: list[TextLine]) -> list[ReadingOrderBlock]:
     all_lines = {TextLineReadingOrder(line) for line in text_lines}
